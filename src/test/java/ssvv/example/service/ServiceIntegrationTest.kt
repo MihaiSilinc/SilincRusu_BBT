88ee6a53caff186b653984a1eed3cda6ee81b161
mihaisilinc20 <mihai.silinc@archive360.com>
2022-04-19 18:55:59 +0300

    x commit

--- src/test/java/ssvv/example/service/ServiceIntegrationTest.kt
+++ src/test/java/ssvv/example/service/ServiceIntegrationTest.kt
@@ -1,6 +1,5 @@
 package ssvv.example.service
 
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -11,7 +10,6 @@ import ssvv.example.repository.TemaXMLRepository
 import ssvv.example.validation.NotaValidator
 import ssvv.example.validation.StudentValidator
 import ssvv.example.validation.TemaValidator
-import java.io.IOException
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -59,34 +57,6 @@ internal class ServiceIntegrationTest {
         )
     }
 
-    @AfterEach
-    @Throws(IOException::class)
-    fun tearDown() {
-
-    }
-
-    @Test
-    fun integrateAddGrade() {
-        try {
-            service.addStudent("123", "boalfa", 112)
-            Assertions.assertEquals(service.findAllStudents().toList().size, 1)
-        } catch (_: Exception) {
-        }
-        try {
-            service.addAssignment("123", "123", 2, 1)
-            Assertions.assertEquals(service.findAllGrades().toList().size, 1)
-        } catch (_: MyException) {
-            Assertions.fail()
-        }
-        try {
-            service.addGrade("123", "123", 9.0, 1, "Da")
-
-            Assertions.assertEquals(service.findAllGrades().toList().size, 1)
-        } catch (_: MyException) {
-            Assertions.fail()
-        }
-    }
-
     @Test
     fun addStudent_validStudent_addsTheStudent() {
         try {
@@ -106,7 +76,34 @@ internal class ServiceIntegrationTest {
         }
     }
 
-    fun addGrade_validGrade_addsTheGrade() {
+    @Test
+    fun integrateAddAssignment() {
+        try {
+            service.addStudent("123", "boalfa", 112)
+            Assertions.assertEquals(service.findAllStudents().toList().size, 1)
+        } catch (_: Exception) {
+        }
+        try {
+            service.addAssignment("123", "123", 2, 1)
+            Assertions.assertEquals(service.findAllGrades().toList().size, 1)
+        } catch (_: MyException) {
+            Assertions.fail()
+        }
+    }
+
+    @Test
+    fun integrateAddGrade() {
+        try {
+            service.addStudent("123", "boalfa", 112)
+            Assertions.assertEquals(service.findAllStudents().toList().size, 1)
+        } catch (_: Exception) {
+        }
+        try {
+            service.addAssignment("123", "123", 2, 1)
+            Assertions.assertEquals(service.findAllGrades().toList().size, 1)
+        } catch (_: MyException) {
+            Assertions.fail()
+        }
         try {
             service.addGrade("123", "123", 9.0, 1, "Da")
 
